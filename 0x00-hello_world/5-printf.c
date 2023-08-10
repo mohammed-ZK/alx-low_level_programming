545a7eab4eaa9ede31216fc522e671de9623bee3
mohammed-ZK <mohammedzainalkouz@gmail.com>
2023-08-10 14:43:10 -0700

    this seventh commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progrm to print with printf function
+ *
+ * return: always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
